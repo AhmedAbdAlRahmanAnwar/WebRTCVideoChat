f0f83f0a879fe4103011e5a4dc6306c0f420b629
AhmedAbdAlRahmanAnwar <ahmedabdalrahman61@gmail.com>
2020-08-06 03:59:07 +0200

    Add files via upload

--- server/server.js
+++ server/server.js
@@ -2,8 +2,8 @@ var app = require("express")();
 var server = require("http").Server(app);
 var io = require("socket.io")(server);
 
-const PORT = 8000;
-//const PORT = process.env.PORT;
+//const PORT = 8000;
+const PORT = process.env.PORT;
 server.listen(PORT);
 
 io.on("connection", function (socket) {
